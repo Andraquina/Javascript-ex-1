56240882c87172434e17a8c4e6e36997bf7777eb
Andraquina <115549652+Andraquina@users.noreply.github.com>
2022-12-06 12:36:31 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let name = "André";
+
+
+
+
+
+let age = "18";
+
+
+
+
+let multiply = age *7
+
+
+
+
+
+let friends = [
+    {
+        name: "Pedrocas",
+        age: 19,
+        isStudent: true
+    },
+
+    {
+        name: "Davidvsky",
+        age: 18,
+        isStudent: true
+    },
+
+    {
+        name: "Zézocas",
+        age: 21,
+        isStudent: true
+    }
+]
+
+
+console.log(name, age, multiply, friends)
+
+
+
+
+
